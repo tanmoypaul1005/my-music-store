9cb82aad64c5b8ebd4560d9823605b885ae9d4be
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2025-11-06 10:23:37 +0600

    Refactor IndexTrends component to define trendsInfo within the component and improve code readability

--- components/index/trends/IndexTrends.tsx
+++ components/index/trends/IndexTrends.tsx
@@ -1,10 +1,8 @@
 "use client"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation, Mousewheel, EffectFade } from 'swiper/modules';
-
 import { useAppStore } from '@/store/app-store';
 import IndexTrendsSlide from './IndexTrendsSlide';
-
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
@@ -15,18 +13,69 @@ import styles from './IndexTrends.module.scss'
 const PLAY_LIST_ID = "top-music-trends-playlist"
 
 const IndexTrends = ({
-    trendsInfo
-} : {
+
+}: {
     trendsInfo: Music[]
 }) => {
+
+    const trendsInfo = [
+        {
+            "id": 120,
+            "name": "LEVEL FIVE - TUMI",
+            "src": "/musics/bangla/LEVEL FIVE - TUMI.mp3",
+            "artist": "Jayati Chakraborty",
+            "coverImage": "/images/music/cover.webp",
+            "avatar": "/images/music/without-me.webp",
+            "playedCount": 63000000
+        },
+        {
+            "id": 121,
+            "name": "Without Me",
+            "src": "/musics/trends/without me.mp3",
+            "artist": "Halsey",
+            "coverImage": "/images/music/cover.webp",
+            "avatar": "/images/music/without-me.webp",
+            "playedCount": 63000000
+        },
+        {
+            "id": 122,
+            "name": "7 rings",
+            "src": "/musics/trends/7 rings.mp3",
+            "artist": "Ariana Grande",
+            "coverImage": "/images/music/cover-2.webp",
+            "avatar": "/images/music/7-rings.webp",
+            "playedCount": 73000000
+        },
+        {
+            "id": 123,
+            "name": "Only Human",
+            "src": "/musics/trends/only human.mp3",
+            "artist": "Jonas Brothers",
+            "coverImage": "/images/music/cover-3.webp",
+            "avatar": "/images/music/only-human.webp",
+            "playedCount": 25000000
+        },
+        {
+            "id": 124,
+            "name": "Attention",
+            "src": "/musics/trends/attention.mp3",
+            "artist": "Charlie Puth",
+            "coverImage": "/images/music/cover-4.webp",
+            "avatar": "/images/music/attention.webp",
+            "playedCount": 110000000
+        }
+    ]
+
     const [music, setMusic, setPlaylist, playlistId] = useAppStore(state => [state.currentMusic, state.setMusic, state.setPlaylist, state.playListId])
 
     const playMusicClickHandler = (music: Music) => {
         setMusic(music)
-        if ( playlistId !== PLAY_LIST_ID ) {
+        if (playlistId !== PLAY_LIST_ID) {
             setPlaylist(PLAY_LIST_ID, trendsInfo)
         }
     }
+
+    console.log('trendsInfo', trendsInfo)
 
     return (
         <Swiper
