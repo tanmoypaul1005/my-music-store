bbe89cbcd1431f1b91520bc759ff2ebd63b2b08d
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2025-11-11 13:05:20 +0600

    fix(player): conditionally render player component based on pathname

--- components/player/Player.tsx
+++ components/player/Player.tsx
@@ -1,25 +1,36 @@
 "use client"
 import { useAppStore } from '@/store/app-store';
-
 import PlayerInfo from './PlayerInfo';
 import PlayerControl from './PlayerControl';
-
 import styles from './Player.module.scss'
 import { logo } from '@/public/images';
+import { usePathname } from 'next/navigation';
 
 const Player = () => {
+
+    const pathName = usePathname();
+
+    console.log("pathName", pathName);
+
     const music = useAppStore(state => state.currentMusic)
 
-    return <div className={styles.player}>
-        <PlayerControl music={music} />
-        <PlayerInfo
-            image={music ? music.avatar : logo}
-            imageAlt={music ? music.name : "not found"}
-            name={music ? music.name : "Not found any music selected"}
-            artist={music ? music.artist : "No One..."}
-            musicId={music ? music.id : null}
-        />
-    </div>
+    return <>
+    {
+        pathName?.includes('/video') ? null : (
+            <div className={styles.player}>
+            <PlayerControl music={music} />
+            <PlayerInfo
+                image={music ? music.avatar : logo}
+                imageAlt={music ? music.name : "not found"}
+                name={music ? music.name : "Not found any music selected"}
+                artist={music ? music.artist : "No One..."}
+                musicId={music ? music.id : null}
+            />
+        </div>
+        )
+    }
+        
+    </>
 }
 
 export default Player;
